38f9618ebd8d3789f80ab440f085ace0389597fd
Marina0203 <marina_hanf@icloud.com>
2024-05-28 17:16:31 +0200

    [FEATURE] Design update
    - implemented design from documentation
    - added default sorting for rooms
    - updated database dump

--- packages/sz_assets/Classes/Domain/Repository/RoomRepository.php
+++ packages/sz_assets/Classes/Domain/Repository/RoomRepository.php
@@ -4,5 +4,8 @@ namespace SUNZINET\SzAssets\Domain\Repository;
 
 class RoomRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 {
-
+    // default sorting
+    protected $defaultOrderings = [
+        'sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+    ];
 }
